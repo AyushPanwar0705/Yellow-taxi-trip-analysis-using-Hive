81b6ebe6eeb2e7ea8d96a8f5c84889719738c115
Ayush Panwar <115878989+AyushPanwar0705@users.noreply.github.com>
2024-03-06 11:07:21 +0530

    Create Creation of Hive table.sql

---
+++ Creation of Hive table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE taxi_trips (
+    vendor_id STRING,
+    pickup_datetime STRING,
+    dropoff_datetime STRING,
+    passenger_count INT,
+    trip_distance DECIMAL(9,6),
+    pickup_longitude DECIMAL(9,6),
+    pickup_latitude DECIMAL(9,6),
+    rate_code INT,
+    store_and_fwd_flag STRING,
+    dropoff_longitude DECIMAL(9,6),
+    dropoff_latitude DECIMAL(9,6),
+    payment_type STRING,
+    fare_amount DECIMAL(9,6),
+    extra DECIMAL(9,6),
+    mta_tax DECIMAL(9,6),
+    tip_amount DECIMAL(9,6),
+    tolls_amount DECIMAL(9,6),
+    total_amount DECIMAL(9,6),
+    trip_time_in_secs INT
+);
